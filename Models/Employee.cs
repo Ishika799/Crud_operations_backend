1397afe72fa82915df5a04fea743c6a3c6aeb9a3
Ishika799 <87866744+Ishika799@users.noreply.github.com>
2024-03-13 14:25:31 +0530

    Add files via upload

---
+++ Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace CrudOperations_backend.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Age { get; set; }
+        public int IsActive { get; set; }
+    }
+}
